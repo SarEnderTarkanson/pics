7c8b26ddfc5bff140dfd7606135702c7e1671fee
Alparslan Tarkan <atarkan@hotmail.com>
2026-01-02 15:39:11 +0100

    68 - showing a scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 void main() {
   var app = MaterialApp(
-    home: Text('Hi There!'),
+    home: Scaffold(
+      appBar: AppBar(),
+    ),
   );
   runApp(app);
 }
