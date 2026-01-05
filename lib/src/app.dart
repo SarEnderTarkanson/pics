45d15bd242646ead49d2a93756171a2d2ce65bc2
Alparslan Tarkan <atarkan@hotmail.com>
2026-01-05 18:15:51 +0100

    76 - the build method

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
-  
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            print('Hi there!');
+          },
+        ),
+        appBar: AppBar(title: Text('Lets see some images!')),
+      ),
+    );
+  }
 }
